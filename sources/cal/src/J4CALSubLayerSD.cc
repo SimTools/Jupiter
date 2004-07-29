4b9657298e6b8e556527e44d5be4b105c4ae25a0
ono <>
2004-07-29 15:22:21 +0000

    2004/07/30 ONO Hiroaki  Input SDs to sublayers

--- sources/cal/src/J4CALSubLayerSD.cc
+++ sources/cal/src/J4CALSubLayerSD.cc
@@ -11,10 +11,16 @@
 //*************************************************************************
 
 #include "J4CALCone.hh"
-#include "J4CALSubLayerSD.hh"
-#include "J4CALSubLayer.hh"
+#include "J4CALTower.hh"
+#include "J4CALBlock.hh"
+#include "J4CALMiniCone.hh"
 #include "J4CALMiniTower.hh"
+#include "J4CALLayer.hh"
+#include "J4CALHit.hh"
+#include "J4CALSubLayer.hh"
+#include "J4CALSubLayerSD.hh"
 #include <cmath>
+#include "J4Timer.hh"
  
 //=====================================================================
 //---------------------
@@ -27,8 +33,9 @@
 J4CALSubLayerSD::J4CALSubLayerSD(J4VDetectorComponent* detector)
 		   :J4VSD<J4CALHit>(detector)
 {  
-  //   fNsectionsInPhi = ((J4CALEM *)detector)->GetNsectionsInPhi();
-  //   fCalHits = new J4CALHit* [fNsectionsInPhi];
+  fTowerNClones = ( (J4CALSubLayer *)detector )->GetTowerNClones();
+
+  fCalHits = new J4CALHit* [fTowerNClones];
 }
 
 //=====================================================================
@@ -36,32 +43,31 @@ J4CALSubLayerSD::J4CALSubLayerSD(J4VDetectorComponent* detector)
 
 J4CALSubLayerSD::~J4CALSubLayerSD()
 {
-//#ifndef __GEANT452__
-//   if (fCalHits) {
-//      for (G4int i=0; i<fNsectionsInPhi; i++) {
-//         if (fCalHits[i]) delete fCalHits[i];
-//      }
-//      delete[] fCalHits;
-//   }
-//#endif
+#ifndef __GEANT452__
+   if (fCalHits[fTowerNClones]) {
+     delete[] fCalHits;
+   }
+#endif
 }
 
 //=====================================================================
 //* Initialize --------------------------------------------------------
 
-void J4CALSubLayerSD::Initialize(G4HCofThisEvent* HCTE)
+void J4CALSubLayerSD::Initialize( G4HCofThisEvent* HCTE )
 {
   //create hit collection(s) and
   //push H.C. to "Hit Collection of This Event"
   
   MakeHitBuf(HCTE);  
 
-  //  for (G4int i=0; i<fNsectionsInPhi; i++) {
-  //if (fCalHits[i]) fCalHits[i] = 0;   // Don't delete fCalHits[i]!
-                                         // Geant4 deletes hit objects at
-                                         // the end of the run.
-  //}
-
+  // Don't "delete" fCalHits[i]!
+  // Geant4 deletes hit objects at
+  // the end of the run.
+  for (G4int i = 0; i < fTowerNClones; i++) {
+    if ( fCalHits[i] ) {
+         fCalHits[i]  = 0;
+   }
+  }
 }
 
 //=====================================================================
@@ -70,57 +76,116 @@ void J4CALSubLayerSD::Initialize(G4HCofThisEvent* HCTE)
 G4bool J4CALSubLayerSD::ProcessHits(G4Step* aStep, G4TouchableHistory* ROhist)
 {
   //In order to use Get function, you must call SetNewStep() at first.
-  
   SetNewStep(aStep);
-   
-  // Check CALSubLayer ID
- 
-  G4int          motherdepth      = 1;
-  G4int          grandmotherdepth = 2;
-  //G4int        daughterdepth    = -1;
 
-  J4VComponent*  component       = GetComponent();                 // SubLayer 
-  J4VComponent*  mothercomp      = GetComponent(motherdepth);      // Tower
-  J4VComponent*  grandmothercomp = GetComponent(grandmotherdepth); // Cone 
-  G4int          myPhiNumber     = GetCloneID(mothercomp); 
-  G4int          myThetaNumber   = grandmothercomp->GetMyID(); 
-  G4int          mothertrackID   = GetMotherTrackID(); 
-  G4int          trackID         = GetTrackID(); 
-  //G4bool         isbarrel        = ((J4CALMiniCone *)grandmothercomp)->IsBarrel();
-  G4bool         isbarrel        = ((J4CALCone *)grandmothercomp)->IsBarrel();
-  G4bool         isSubLayer      = TRUE; 
+  // Check SubLayer ID
+  ////////////////////////////////////////////////////////////////
+  // Calorimeter sub structure and depth-------------------------
+  //
+  // SubLayer->Layer->MiniTower->MiniCone->Block->Tower->Cone->CAL
+  ////////////////////////////////////////////////////////////////
+ // G4int SubLayerDepth  = 0;
+  const G4int LayerDepth     = 1;
+  const G4int MiniTowerDepth = 2;
+  const G4int MiniConeDepth  = 3;
+  const G4int BlockDepth     = 4;
+  const G4int TowerDepth     = 5;
+  const G4int ConeDepth      = 6;
+
+  // poiters for components -------------------------------------
+  J4VComponent*  ptrSubLayerComponent  = GetComponent();
+  J4VComponent*  ptrLayerComponent     = GetComponent(LayerDepth);
+  J4VComponent*  ptrMiniTowerComponent = GetComponent(MiniTowerDepth);
+  J4VComponent*  ptrMiniConeComponent  = GetComponent(MiniConeDepth);
+  J4VComponent*  ptrBlockComponent     = GetComponent(BlockDepth);
+  J4VComponent*  ptrTowerComponent     = GetComponent(TowerDepth);
+  J4VComponent*  ptrConeComponent      = GetComponent(ConeDepth);
+
+  G4int  myConeID      = ptrConeComponent      -> GetMyID();
+  G4int  myTowerID     = GetCloneID( ptrTowerComponent );
+  G4int  myMiniConeID  = ptrMiniConeComponent  -> GetMyID();
+  G4int  myMiniTowerID = ptrMiniTowerComponent -> GetMyID();
+  G4int  myLayerID     = ptrLayerComponent     -> GetMyID();
+  G4int  mySubLayerID  = ptrSubLayerComponent  -> GetMyID();
+
+  G4int  trackID       = GetTrackID();
+  G4int  mothertrackID = GetMotherTrackID(); 
+  
+  G4bool isBarrel      = ( (J4CALCone *)ptrConeComponent ) -> IsBarrel();
+  const G4String &firstName = ( (J4CALBlock *)ptrBlockComponent ) -> GetFirstName();
+
+  G4bool isEM; 
+  if ( firstName == "EM" ) {
+     isEM = TRUE;
+  } else { 
+     isEM = FALSE;
+  }
 
   //Get perticle information
-  G4double               tof           = GetTof();
-  G4double               edep          = GetEnergyDeposit();
-  const G4ThreeVector   &pre           = GetPrePosition();
-  G4ParticleDefinition  *particle      = GetParticle(); 
-    
+  G4double               tof      = GetTof();
+  G4double               edep     = GetEnergyDeposit();
+  const G4ThreeVector   &pre      = GetPrePosition();
+  G4ParticleDefinition  *particle = GetParticle(); 
+
+#if 0
+  G4cerr << ptrSubLayerComponent -> GetName() << " "
+         << G4endl;
+#endif
+
+#if 0
+  G4cerr << "fTowerNCloens = " << fTowerNClones 
+         << ", fMiniTowerNClones = " << fMiniTowerNClones << G4endl;
+
+  G4cerr << "ConeID : "      << myConeID << " "
+         << "TowerID : "     << myTowerID << " "
+         << "MiniConeID : "  << myMiniConeID << " " 
+         << "MiniTowerID : " << myMiniTowerID << " " 
+         << "LayerID : "     << myLayerID << " " 
+         << "SubLayerID : "  << mySubLayerID << " " 
+         << "ClonesID : "    << myTowerID << " " 
+         << "TrackID : "     << trackID << " " 
+         << "motherTrackID : " << mothertrackID << " " 
+         << "isBarrel : "    << isBarrel << " " 
+         << "isEM : "        << isEM << " " 
+         << "TOF : "         << tof << " "
+         << "edep : "        << edep << " "
+         << "pre  : "        << pre << " "
+         << "particle  : "   << particle->GetParticleName() << " "
+         << G4endl;
+#endif
+
   // Create new hit
+  //IncrementHitNumber();
 
-  if (!fCalHits[myPhiNumber]) {
+  if ( !fCalHits[myTowerID] ) {
 
-     // create new hit!
-     fCalHits[myPhiNumber] = new J4CALHit(component, myThetaNumber, myPhiNumber,  
-                                         isbarrel, isSubLayer,  edep, tof, pre, mothertrackID, 
-                                         trackID, particle);
-     ((J4CALHitBuf *)GetHitBuf())->insert(fCalHits[myPhiNumber]);
+    // create new hit!
+    fCalHits[myTowerID] = new J4CALHit(
+                                                ptrSubLayerComponent,
+                                                 myConeID, myTowerID,
+                                         myMiniConeID, myMiniTowerID,
+                                             myLayerID, mySubLayerID,
+                                                      isBarrel, isEM,
+                                       edep, tof, pre, mothertrackID,
+                                                   trackID, particle );
+
+   ((J4CALHitBuf *)GetHitBuf())->insert( fCalHits[myTowerID] );
 
   } else {
 
-     fCalHits[myPhiNumber]->AccumulateEdep(edep);
+     fCalHits[myTowerID]->AccumulateEdep(edep);
 
-     if (fCalHits[myPhiNumber]->GetMotherTrackID() > mothertrackID &&
-         fCalHits[myPhiNumber]->GetTof() > tof) {
+     if ( fCalHits[myTowerID]->GetMotherTrackID() > mothertrackID
+          &&fCalHits[myTowerID]->GetTof() > tof)
+        {
          // replace information of first hit
-         fCalHits[myPhiNumber]->SetTof(tof);
-         fCalHits[myPhiNumber]->SetInjectionPoint(pre);
-         fCalHits[myPhiNumber]->SetMotherTrackID(mothertrackID);
-         fCalHits[myPhiNumber]->SetTrackID(trackID);
-         fCalHits[myPhiNumber]->SetParticle(particle);
+         fCalHits[myTowerID]->SetTof(tof);
+         fCalHits[myTowerID]->SetInjectionPoint(pre);
+         fCalHits[myTowerID]->SetMotherTrackID(mothertrackID);
+         fCalHits[myTowerID]->SetTrackID(trackID);
+         fCalHits[myTowerID]->SetParticle(particle);
      }
   }   
-
   return TRUE;
 }
 
@@ -148,7 +213,7 @@ void J4CALSubLayerSD::DrawAll()
 void J4CALSubLayerSD::PrintAll()
 {
   G4int nHit= ((J4CALHitBuf*)GetHitBuf())-> entries();
-  std::cout << "------------------------------------------" << std::endl
-         << "*** tracker Hit (#hits=" << nHit << ")" << std::endl;
-  ((J4CALHitBuf*)GetHitBuf())-> PrintAllHits();
+  G4cout << "------------------------------------------" << G4endl; 
+  G4cout << "*** tracker Hit (#hits=" << nHit << ")" <<G4endl;
+  ( (J4CALHitBuf*)GetHitBuf() )-> PrintAllHits();
 }
