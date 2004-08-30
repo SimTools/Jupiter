0ba6433743028c019ca00f58558d744feeade635
ono <>
2004-08-30 20:01:03 +0000

    2004/08/31  Ono Hiroaki Add CALSD getting CAL front data

---
+++ sources/cal/include/J4CALSD.hh
@@ -0,0 +1,73 @@
+// $Id$
+#ifndef __J4CALSD__
+#define __J4CALSD__
+//*************************************************************************
+//* --------------------
+//* J4CALSD
+//* --------------------
+//* (Description)
+//* 	It appends Sensitive propaty to J4CALEM. 
+//*	Mother class : J4VSD <- G4VSensitiveDetector
+//*     
+//* (Update Record)
+//*	2003/03/18  K.Hoshina	Original version.
+//*************************************************************************
+ 
+#include "J4VSD.hh"
+#include "J4CALPreHit.hh"
+
+//=====================================================================
+//---------------------
+// class definition
+//---------------------
+
+class J4CALSD : public J4VSD<J4CALPreHit>{
+  
+public:
+
+  J4CALSD( J4VDetectorComponent* detector );
+  ~J4CALSD();
+
+  virtual G4bool ProcessHits( G4Step* aStep, G4TouchableHistory* ROhist );
+  virtual void   Initialize ( G4HCofThisEvent* PreHCTE );
+  virtual void   EndOfEvent ( G4HCofThisEvent* PreHCTE );
+
+  virtual void   DrawAll();
+  virtual void   PrintAll(); 
+ 
+  virtual void   OutputAll( G4HCofThisEvent* PreHCTE )
+  {
+    if ( GetHitBuf() )
+    {
+      J4VSD<J4CALPreHit>::OutputAll( PreHCTE );
+    }
+    else
+    {
+      G4cerr << "J4CALSD::OutputAll: No HitBuf! " << G4endl;
+    }
+  }
+
+  inline virtual const J4CALSD& operator=( const J4VSD<J4CALPreHit> &right )
+  {
+    J4VSD<J4CALPreHit>::operator=( right );
+    return *this;
+  }
+  
+  // set/get functions
+  inline J4CALPreHit* GetCurrentPreHitPtr() const { return fCurrentPreHitPtr; }
+  inline static G4int GetCurrentPreHitID() { return fgCurrentPreHitID; }
+  inline static G4int GetCurrentTrackID()  { return fgCurrentTrackID; }
+
+private:
+  inline void SetCurrentPreHitPtr( J4CALPreHit* p ) { fCurrentPreHitPtr = p; }
+  inline static void SetCurrentPreHitID( G4int n )  { fgCurrentPreHitID = n; }
+  inline static void SetCurrentTrackID( G4int n )   { fgCurrentTrackID  = n; }
+   
+private:
+  //J4CALPreHit  *fCalPreHits;
+  J4CALPreHit*  fCurrentPreHitPtr;
+  static G4int  fgCurrentPreHitID;
+  static G4int  fgCurrentTrackID;
+};
+
+#endif
