77b72416d093fb314f8aadd04b7d054b6b6614e6
ono <>
2004-08-30 19:54:47 +0000

    2004/08/31 Ono Hiroaki  Add new Hits include hitlets

--- sources/cal/src/J4CALHit.cc
+++ sources/cal/src/J4CALHit.cc
@@ -13,57 +13,47 @@
 
 #include <iomanip>
 #include "J4CALHit.hh"
+#include "TVAddress.hh"
+//#include "J4CALHitlet.hh"
+//#include "J4CALPreHit.hh"
 
 //=========================================================================
 //---------------------
 // Allocator
 //---------------------
-
-J4CALHitAllocator J4CALHit::fHitAllocator ;
-J4Output*         J4CALHit::fOutput = 0 ;
+J4CALHitAllocator J4CALHit::fgHitAllocator;
+J4Output*         J4CALHit::fgOutput = 0;
  
 //=========================================================================
 //* constructor -----------------------------------------------------------
-J4CALHit::J4CALHit(
-                   J4VComponent         *detector,       // He is in "location" now
-                   G4int                 ConeID,         // Cone ID  
-                   G4int                 TowerID,        // Tower ID
-                   G4int                 MiniConeID,     // MiniCone ID 
-                   G4int                 MiniTowerID,    // MiniTower ID 
-                   G4int                 LayerID,        // Layer ID 
-                   G4int                 SubLayerID,     // SubLayer ID 
-                   G4bool                isBarrel,       // (0, 1) = (endcap, barrel) 
-                   G4bool                isEM,           // (0, 1) = (HD, EM) 
-                   G4double              edep,           // Energy Deposit
-                   G4double              tof,            // TOF 
-                   const G4ThreeVector  &pre,            // Pre-position of track
-                   G4int                 mothertrackID,  // Mother track id
-                   G4int                 trackID,        // My track id 
-                   G4ParticleDefinition *particle )      // My particle definision 
- : J4VHit(detector), 
-   fConeID(ConeID), fTowerID(TowerID),
-   fMiniConeID(MiniConeID), fMiniTowerID(MiniTowerID), 
-   fLayerID(LayerID), fSubLayerID(SubLayerID),
-   fIsBarrel(isBarrel), fIsEM(isEM),
-   fEnergyDep(edep), fTof(tof), fInjectionPoint(pre),
-   fMotherTrackID(mothertrackID), fTrackID(trackID), fParticle(particle)
+J4CALHit::J4CALHit()
+  : J4VHit(0), /*fPreHitID(0),*/ fAddress(0), fCellEdep(0), fCellTof(0), fCellXcm(0)
+{
+}
+
+J4CALHit::J4CALHit( J4VComponent* ptrDetector,    // He is in "location" now
+		    //	    G4int         preHitID,       // pre Hit ID
+      		    TVAddress*    ptrAddress,     // ptr to cell address
+                    G4double      edep,           // Energy Deposit
+                    G4double      tof,            // TOF
+		    const G4ThreeVector& Xcm  )		    
+  : J4VHit( ptrDetector ), /* fPreHitID( preHitID ),*/ fAddress( ptrAddress ),
+    fCellEdep( edep ), fCellTof( tof ), fCellXcm( Xcm )
 {
 }
 
 //=========================================================================
 //* destructor -----------------------------------------------------------
-
 J4CALHit::~J4CALHit()
 {
 }
 
-
 //=========================================================================
 //* Output ------------------------------------------------------------------
 
-void J4CALHit::Output(G4HCofThisEvent* /* HCTE */ )
+void J4CALHit::Output( G4HCofThisEvent* /* HCTE */ )
 {
-  if ( fOutput ) fOutput->Output( this );
+  if ( fgOutput ) fgOutput -> Output( this );
   // output hitdata to output file ....
         
   std::ofstream& ofs = GetOutputFileStream();
@@ -71,27 +61,41 @@ void J4CALHit::Output(G4HCofThisEvent* /* HCTE */ )
     const G4String& errorMessage = "J4CALHit::Output(): write error.";
     G4cerr << errorMessage << G4endl;
   } else {
-     ofs << std::setw(3) << fConeID  << " " 
-         << std::setw(3) << fTowerID << " " 
-         << std::setw(2) << fMiniConeID << " " 
-         << std::setw(2) << fMiniTowerID << " " 
-         << std::setw(3) << fLayerID << " " 
-         << std::setw(2) << fSubLayerID << " " 
-         << std::setw(2) << fIsBarrel << " " 
-         << std::setw(2) << fIsEM << " " 
-         << std::setw(7) << fMotherTrackID << " " 
-         << std::setw(7) << fTrackID << " " 
-         << std::setw(6) << fParticle->GetPDGEncoding() << " " 
-         << std::setw(2) << fParticle->GetPDGCharge() << " " 
-         << std::setiosflags(std::ios::scientific) << std::setprecision(14)
-         << std::setw(18) << fEnergyDep << " " 
-         << std::setw(18) << fTof << " "
-         << std::setw(18) << fInjectionPoint.x() << " " 
-         << std::setw(18) << fInjectionPoint.y() << " " 
-         << std::setw(18) << fInjectionPoint.z() << " "
-         << std::setiosflags(std::ios::floatfield) 
-         << std::setprecision(8)
-         << std::endl;
+
+    for ( G4int i = 0; i < GetNHitlets(); i++ ) {
+      ofs << std::setw(3) << "cell: "
+	  //<< std::setw(i+2) << GetNHitlets() << " "
+	  << std::setw(i+2) << GetPreHitID(i) << " "
+	  << std::setw(3) << fAddress -> GetConeID() << " "
+	  << std::setw(3) << fAddress -> GetTowerID() << " "
+	  << std::setw(3) << fAddress -> GetMiniConeID() << " "
+	  << std::setw(3) << fAddress -> GetMiniTowerID() << " "
+	  << std::setw(3) << fAddress -> GetLayerID() << " "
+	  << std::setw(3) << fAddress -> GetSubLayerID() << " "
+	  << std::setw(3) << fAddress -> IsBarrel() << " "
+	  << std::setw(3) << fAddress -> IsEM() << " "
+	  << std::setw(12) << GetHitletEdep(i) << " "
+	  << std::setw(12) << GetHitletTof(i) << " "
+	  << std::setw(12) << GetHitletXcm(i).x()  << " "
+	  << std::setw(12) << GetHitletXcm(i).y()  << " "
+	  << std::setw(12) << GetHitletXcm(i).z()  << " "
+	  << std::endl;
+    }
+    //    ofs /* << std::setw(3) << fPreHitID << " " */
+//	<< std::setw(3) << fAddress -> GetConeID() << " "
+//        << std::setw(3) << fAddress -> GetTowerID() << " "
+//        << std::setw(3) << fAddress -> GetMiniConeID() << " "
+//        << std::setw(3) << fAddress -> GetMiniTowerID() << " "
+//	<< std::setw(3) << fAddress -> GetLayerID() << " "
+//	<< std::setw(3) << fAddress -> GetSubLayerID() << " "
+//	<< std::setw(3) << fAddress -> IsBarrel() << " "
+//	<< std::setw(3) << fAddress -> IsEM() << " "
+//        << std::setw(12) << fCellEdep << " " 
+//        << std::setw(12) << fCellTof << " "
+//	<< std::setw(12) << fCellXcm.x() << " "
+//	<< std::setw(12) << fCellXcm.y() << " "
+//	<< std::setw(12) << fCellXcm.z() << " "
+//        << std::endl;
   }
 }
 
@@ -107,13 +111,10 @@ void J4CALHit::Draw()
 
 void J4CALHit::Print()
 {
-  std::cout << std::setiosflags(std::ios::fixed);
-  std::cout << std::setw(20) << GetComponentName() << " " << std::endl;
-  std::cout << " track#=" << fTrackID
-         << " charge=" << fParticle->GetPDGCharge()
-         << " energy(GeV)= " << std::setprecision(2) 
-         << std::setw(6) << fEnergyDep *(1./GeV) << " "
-         << " TOF(ns)= " << std::setw(4) << std::setprecision(1) 
-         << fTof /ns 
-         << std::endl;  
+  std::cout << std::setw(20) << GetComponentName() << " "
+            << "energy(GeV)= " << std::setprecision(2)
+            << std::setw(6) << fCellEdep *(1./GeV) << " "
+            << "TOF(ns)= " << std::setw(4) << std::setprecision(1) 
+            << fCellTof /ns 
+            << std::endl;  
 }
