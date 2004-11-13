af5147d5da905d56f478e35be270b04dbc9907d7
yamaguti <>
2004-11-13 19:39:30 +0000

    2004/11/14 A.Yamaguchi  Added PostHit

---
+++ sources/tpc/include/J4TPCPostHit.hh
@@ -0,0 +1,115 @@
+#ifndef __J4TPCPOSTHIT__
+#define __J4TPCPOSTHIT__
+//*************************************************************************
+//* --------------------
+//* J4TPCPostHit
+//* --------------------
+//* (Description)
+//*     Hit class for J4TPCLayer 	
+//*	Mother class : G4VHit
+//*     
+//* (Update Record)
+//*	2001/02/18  K.Hoshina	Original version.
+//*************************************************************************
+
+#include "J4VTrackerHit.hh"
+#include "J4Output.hh"
+#include "G4THitsCollection.hh"
+#include "G4HCofThisEvent.hh"
+
+
+//=========================================================================
+// TypeDef
+
+class J4TPCPostHit;
+typedef G4Allocator<J4TPCPostHit> J4TPCPostHitAllocator;
+typedef G4THitsCollection<J4TPCPostHit> J4TPCPostHitBuf;
+
+//=========================================================================
+//---------------------
+// class definition
+//---------------------
+ 
+
+class J4TPCPostHit : public J4VTrackerHit {
+
+public:
+  J4TPCPostHit();
+  J4TPCPostHit(J4VComponent              *detector,
+                    G4int                 trackID       = 0,
+                    G4int                 mothertrackID = 0,
+                    G4ParticleDefinition *particle      = 0,
+                    G4double              tof           = 0,
+                    G4double              totalE        = 0,
+                    const G4ThreeVector  &momentum      = 0,
+                    const G4ThreeVector  &pos           = 0);	
+  	 	            
+  J4TPCPostHit(const J4TPCPostHit& right);
+  
+  virtual ~J4TPCPostHit();
+  
+  const J4TPCPostHit& 
+          operator=(const J4TPCPostHit& right);    
+  void* operator new    (size_t    );
+  void  operator delete (void* aHit);
+
+  virtual void Output(G4HCofThisEvent* HCTE);
+  static void SetCurPostHitPtr(J4TPCPostHit *hitp = 0) { fgCurPostHitPtr = hitp; }
+  static J4TPCPostHit *GetCurPostHitPtr() { return fgCurPostHitPtr; }
+  inline virtual G4int GetPostHitID() const { return fPostHitID; } 
+  static G4int GetCurPostHitID()      { return fgNhits; }
+  static  void SetOutput(J4Output *output) { fgOutput = output; }
+  virtual void Draw();
+  virtual void Print();
+    
+private: 
+
+  static J4TPCPostHitAllocator  fgHitAllocator;
+  static J4Output              *fgOutput;       // Pointer to Output Module
+  static G4int                  fgNhits;
+  static J4TPCPostHit          *fgCurPostHitPtr;
+         G4int                  fPostHitID; 
+};
+
+//=====================================================================
+//----------------------------------------
+// inline function for J4TPCPostHit
+//----------------------------------------
+
+inline J4TPCPostHit::J4TPCPostHit()
+{
+  fPostHitID = ++fgNhits;
+}
+
+inline J4TPCPostHit::J4TPCPostHit(const J4TPCPostHit& right) 
+     : J4VTrackerHit(right)
+{
+  fPostHitID = ++fgNhits;
+}
+
+inline const J4TPCPostHit& 
+  J4TPCPostHit::operator= (const J4TPCPostHit& right)
+{         
+  J4VTrackerHit::operator=(right);
+  return *this;
+}
+  
+//----------------------------------------
+// Allocator
+
+inline void* J4TPCPostHit::operator new(size_t)
+{
+  
+  void* aHit;
+  aHit= (void*)fgHitAllocator.MallocSingle();
+  return aHit;
+}
+
+inline void J4TPCPostHit::operator delete(void* aHit)
+{
+  fgHitAllocator.FreeSingle((J4TPCPostHit*) aHit);
+}
+
+
+#endif
+
---
+++ sources/tpc/src/J4TPCPostHit.cc
@@ -0,0 +1,122 @@
+//*************************************************************************
+//* --------------------
+//* J4TPCPostHit
+//* --------------------
+//* (Description)
+//* 	Derivated concreate hit class for J4TPCDriftRegion.
+//*	Mother class : J4VHit
+//*     
+//* (Update Record)
+//*	2004/11/13  A.Yamaguchi	Original version.
+//*************************************************************************
+
+#include <iomanip>
+#include "J4TPCPostHit.hh"
+
+//=========================================================================
+//---------------------
+// Allocator
+//---------------------
+
+J4TPCPostHitAllocator J4TPCPostHit::fgHitAllocator;
+J4Output*             J4TPCPostHit::fgOutput = 0;
+G4int                 J4TPCPostHit::fgNhits = 0;
+J4TPCPostHit         *J4TPCPostHit::fgCurPostHitPtr = 0;
+
+
+//=========================================================================
+//* constructor -----------------------------------------------------------
+
+J4TPCPostHit::J4TPCPostHit(
+                   J4VComponent         *detector,       // He is in "location" now
+                   G4int                 trackID,        // TrackID
+                   G4int                 mothertrackID,  // MotherTrackID
+                   G4ParticleDefinition *particle,       // particle
+                   G4double              tof,            // TOF 
+                   G4double              totalE,         // Total energy
+                   const G4ThreeVector  &momentum,       // Momentum of perticle
+                   const G4ThreeVector  &pos )           // Post-position of track
+             : J4VTrackerHit(detector, trackID, mothertrackID, particle,
+                      tof, 0., totalE, momentum, pos, pos)
+{
+  fPostHitID = ++fgNhits;
+}
+
+//=========================================================================
+//* destructor -----------------------------------------------------------
+
+J4TPCPostHit::~J4TPCPostHit()
+{
+  fgNhits--;
+}
+
+
+//=========================================================================
+//* Output ------------------------------------------------------------------
+
+void J4TPCPostHit::Output(G4HCofThisEvent *)
+{
+  if (fgOutput) fgOutput->Output(this);
+  // output hitdata to output file ....
+        
+  std::ofstream& ofs = GetOutputFileStream();
+  if (! ofs.good()) {
+    G4String errorMessage= "J4TPCPostHit::Output(): write error.";
+    G4Exception(errorMessage);
+  } else {
+
+     G4ThreeVector pre  = GetPrePosition();
+     G4ThreeVector post = GetPostPosition();
+
+     ofs << std::setw(7) << fPostHitID << " "
+         << std::setw(1) << GetComponent()->GetMyID() << " " 
+         << std::setw(7) << GetTrackID() << " " 
+         << std::setw(7) << GetMotherTrackID() << " " 
+         << std::setw(6) << GetPDGEncoding() << " " 
+         << std::setw(2) << GetCharge() << " " 
+         << std::setiosflags(std::ios::scientific) << std::setprecision(14)
+         << std::setw(18) << pre.x() << " " 
+         << std::setw(18) << pre.y() << " " 
+         << std::setw(18) << pre.z() << " "
+         << std::setw(18) << post.x() << " " 
+         << std::setw(18) << post.y() << " " 
+         << std::setw(18) << post.z() << " "
+         << std::setw(18) << GetMomentum().x() << " " 
+         << std::setw(18) << GetMomentum().y() << " "
+         << std::setw(18) << GetMomentum().z() << " " 
+         << std::setw(18) << GetTotalEnergy() << " " 
+         << std::setw(18) << GetEnergyDeposit() << " " 
+         << std::setw(18) << GetTof() << " "
+         << std::setiosflags(std::ios::floatfield) 
+         << std::setprecision(8)
+         << G4endl;
+  }
+
+	
+}
+
+//=========================================================================
+//* Draw ------------------------------------------------------------------
+
+void J4TPCPostHit::Draw()
+{
+}
+
+//=========================================================================
+//* Print -----------------------------------------------------------------
+
+void J4TPCPostHit::Print()
+{
+
+
+  G4cout << std::setiosflags(std::ios::fixed);
+  G4cout << std::setw(20) << GetComponentName() << " " << G4endl;
+  G4cout << " track#=" << GetTrackID()
+         << " charge=" << GetCharge()
+         << " energy(GeV)= " << std::setprecision(2) 
+         << std::setw(6) << GetTotalEnergy() *(1./GeV) << " "
+         << " TOF(ns)= " << std::setw(4) << std::setprecision(1) 
+         << GetTof() /ns 
+         << G4endl;  
+}
+
