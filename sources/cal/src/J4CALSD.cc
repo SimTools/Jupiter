222ff44374dea672693bea987428f3112eba9335
ono <>
2005-02-27 10:10:34 +0000

    2005/02/27   ONO Hiroaki   Add J4CALPostHit

--- sources/cal/src/J4CALSD.cc
+++ sources/cal/src/J4CALSD.cc
@@ -9,16 +9,18 @@
 //* (Update Record)
 //*	2000/12/08  K.Hoshina	Original version.
 //*************************************************************************
-
 #include "J4TPCPostHit.hh"
 #include "J4TPCPostHitKeeper.hh"
 #include "J4CALSD.hh"
 #include "J4CALPreHit.hh"
 #include "J4CALPreHitKeeper.hh"
+#include "J4CALPostHit.hh"
+#include "J4CALPostHitKeeper.hh"
 #include "J4CAL.hh"
 #include "J4CALCone.hh"
 #include "J4VCALMiniTower.hh"
 #include "J4CALEM.hh"
+#include "J4CALParameterList.hh"
 #include "J4TrackingAction.hh"
 #include <cmath>
  
@@ -29,16 +31,15 @@
 
 //=====================================================================
 //* constructor -------------------------------------------------------
-
 J4CALSD::J4CALSD( J4VDetectorComponent* detector )
-                  :J4VSD<J4CALPreHit>( detector )
+ :J4VSD<J4CALPreHit>( detector )
 {
   J4TrackingAction::GetInstance()->Add(J4CALPreHitKeeper::GetInstance());
+  J4TrackingAction::GetInstance()->Add(J4CALPostHitKeeper::GetInstance());
 }
 
 //=====================================================================
 //* destructor --------------------------------------------------------
-
 J4CALSD::~J4CALSD()
 {
   //#ifndef __GEANT452__
@@ -48,7 +49,6 @@ J4CALSD::~J4CALSD()
 
 //=====================================================================
 //* Initialize --------------------------------------------------------
-
 void J4CALSD::Initialize( G4HCofThisEvent* PreHCTE )
 {
   //create hit collection(s) and
@@ -59,26 +59,24 @@ void J4CALSD::Initialize( G4HCofThisEvent* PreHCTE )
   // Don't delete fCalPreHits!
   // Geant4 deletes hit objects at
   // the end of the run.
-
   //if ( fCalPreHits ) fCalPreHits = 0;   
 
 }
 
 //=====================================================================
 //* ProcessHits -------------------------------------------------------
-
 G4bool J4CALSD::ProcessHits( G4Step* aStep, G4TouchableHistory* /* ROhist */ )
 {
   //In order to use Get function, you must call SetNewStep() at first.
-  
   SetNewStep(aStep);
-  
+
   J4VComponent* ptrCAL = GetComponent(0);
 
   //Get perticle information
   //G4int                 preHitID = 0;
   G4int                 postHitID = J4TPCPostHitKeeper::GetInstance()->GetCurPostHitID();
   const G4ThreeVector&  pre       = GetPrePosition();
+  const G4ThreeVector&  post      = GetPostPosition();
   const G4ThreeVector&  momentum  = GetMomentum();
   //  G4double              edep     = GetEnergyDeposit();
   G4double              energy    = GetKineticEnergy();
@@ -87,29 +85,30 @@ G4bool J4CALSD::ProcessHits( G4Step* aStep, G4TouchableHistory* /* ROhist */ )
   G4int                 motherTrackID = GetMotherTrackID();
   G4ParticleDefinition* particle = GetParticle(); 
   
-  // Create new hit
+  // Create new pre hit --------------------------------------
   J4CALPreHitKeeper *phkp = J4CALPreHitKeeper::GetInstance();
   if ( phkp->IsNext() ) { 
-
      phkp->IncrementCurrentPreHitID();
      G4int preHitID = phkp->GetCurrentPreHitID();
-
      // create new hit!
-
-     J4CALPreHit *preHitPtr = new J4CALPreHit( ptrCAL, postHitID, preHitID, pre, momentum, 
-                                               energy, tof, trackID, particle, motherTrackID );
-
+     J4CALPreHit *preHitPtr = new J4CALPreHit( ptrCAL, postHitID, preHitID, pre, momentum, energy, tof, trackID, particle, motherTrackID );
      phkp->SetCurrentPreHitPtr(preHitPtr);
-     
      ( (J4CALPreHitBuf *)GetHitBuf() ) -> insert( preHitPtr );
-          
-  }   
+  }
+
+  // Create new postHit! -------------------------------------
+  J4CALPostHitKeeper* postKeeper = J4CALPostHitKeeper::GetInstance();
+  if ( IsExiting( post, momentum ) ) {
+    G4int calPostHitID = postKeeper->GetCurPostHitID();
+    G4int calPreHitID  = phkp->GetCurrentPreHitID();
+    J4CALPostHit* postHitPtr = new J4CALPostHit( ptrCAL, calPreHitID, calPostHitID, motherTrackID, particle, tof, energy, momentum, post );
+    ( (J4CALPostHitBuf *)GetHitBuf() ) -> insert( postHitPtr );
+  }
   return TRUE;
 }
 
 //=====================================================================
 //* EndOfEvent --------------------------------------------------------
-
 void J4CALSD::EndOfEvent( G4HCofThisEvent* /* PreHCTE */ )
 {			
   J4CALPreHitKeeper *phkp = J4CALPreHitKeeper::GetInstance();
@@ -121,21 +120,32 @@ void J4CALSD::EndOfEvent( G4HCofThisEvent* /* PreHCTE */ )
 //=====================================================================
 //* clear -------------------------------------------------------------
 
-
 //=====================================================================
 //* DrawAll -----------------------------------------------------------
-
-void J4CALSD::DrawAll()
-{
-}
+void J4CALSD::DrawAll() { }
 
 //=====================================================================
 //* PrintAll ----------------------------------------------------------
-
 void J4CALSD::PrintAll()
 {
   G4int nHit= ( (J4CALPreHitBuf*)GetHitBuf() ) -> entries();
   std::cout << "------------------------------------------" << std::endl
             << "*** tracker Hit (#hits=" << nHit << ")" << std::endl;
   ( (J4CALPreHitBuf*)GetHitBuf() ) -> PrintAllHits();
+}
+
+//======================================================================
+//* Is Exiting ---------------------------------------------------------
+G4bool J4CALSD::IsExiting( const G4ThreeVector &pos, const G4ThreeVector &p  ) const
+{
+  J4CALParameterList* ptrList = J4CALParameterList::GetInstance();
+  static G4double tol = 10.;
+    
+  if ( abs( pos.perp() - ptrList->GetCALOuterR() ) <= tol*kCarTolerance && p.x() * pos.x() + p.y() * pos.y() > 0.) 
+    return true;
+
+  if ( abs( abs( pos.z() ) - ptrList->GetCALOuterHalfZ() ) <= tol*kCarTolerance && p.z() * pos.z() > 0.)
+     return true;
+
+  return false;
 }
