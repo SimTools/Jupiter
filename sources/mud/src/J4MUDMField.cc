74a72580b684bbfd2895406251e16c64e1d5e03c
ono <>
2005-06-03 16:27:16 +0000

    2005/06/04 H.Ono  Change magnetic field map

--- sources/mud/src/J4MUDMField.cc
+++ sources/mud/src/J4MUDMField.cc
@@ -15,7 +15,7 @@
 
 //=====================================================================
 //* constructor -------------------------------------------------------
-J4MUDMField::J4MUDMField(J4MUDParameterList* list)
+J4MUDMField::J4MUDMField( J4MUDParameterList* list )
  : J4VMField(), fParameterList( list )
 {}
 
@@ -29,44 +29,79 @@ void J4MUDMField::GetLocalFieldValue( G4ThreeVector &position,
                                       G4bool        &onlyFlag )
 {
   onlyFlag = false;
-	
-  G4double mudR         = fParameterList->GetMUDOuterR();
-  G4double innerR       = fParameterList->GetMUDEndcapInnerR();
-  G4double frontEndcapR = fParameterList->GetFrontEndcapOuterR();
-  G4double barrelR      = fParameterList->GetBarrelInnerR();
-  G4double barrelZ      = fParameterList->GetBarrelFrontHalfL();
-  G4double mudZ         = fParameterList->GetMUDHalfL();
-  G4double magField     = fParameterList->GetBField();
-  G4double posZ         = std::abs( position.z() );
-  G4double posR         = sqrt( sqr( position.x() ) + sqr( position.y() ) );
+
+  G4double mudR            = fParameterList->GetMUDOuterR();
+  G4double innerR          = fParameterList->GetMUDEndcapInnerR();
+  G4double frontEndcapR    = fParameterList->GetFrontEndcapOuterR();
+  G4double barrelR         = fParameterList->GetBarrelInnerR();
+  G4double barrelZ         = fParameterList->GetBarrelFrontHalfL();
+  G4double mudZ            = fParameterList->GetMUDHalfL();
+  G4double posZ            = std::abs( position.z() );
+  G4double posR            = sqrt( sqr( position.x() ) + sqr( position.y() ) );
+  G4double diagonalTan     = mudR / mudZ;
+  G4double endcapTan       = frontEndcapR / ( mudZ - barrelZ );
+
+  G4double barrelField     = fParameterList->GetBarrelBField();
+  G4double cornerField     = fParameterList->GetCornerBField();  
+  G4double endcapField     = fParameterList->GetEndcapBField();
+  G4double returnField     = fParameterList->GetReturnBField();
+
+  const G4ThreeVector& endcapPos     = position - position.project(); // pos_xy = pos - pos_z
+  const G4ThreeVector& endcapPosUnit = endcapPos.unit();              // Unit vector parellel of pos_xy
+  const G4ThreeVector& bEndcap       = endcapField * endcapPosUnit;   // 
 
   //**** Magentic field direction/magnitude setup
 
-  // Barrel part outer radius position of the SOL, B_z direction < 0 
+  // Barrel part to diagonal part
+  // B_vec = B*( 0, 0, -1 )
   if ( posZ < barrelZ && posR > barrelR && posR < mudR ) {
-    //**** Opposit direction to the SOL bfield
-    bfield.setZ( -magField );
+   bfield.setX( 0. );
+   bfield.setY( 0. );
+    bfield.setZ( -barrelField );
   }
   
-  // Endcap part, outer position of SOL, z position > 0, B_y direction > 0  
-  if ( position.z() > 0 && posZ > barrelZ && posZ < mudZ && posR > frontEndcapR && posR < mudR ) {
-    bfield.setY( magField );
+  // Barrel part to diagonal part
+  // B_vec = B*( 0, 0, -1 )
+  if ( posZ > barrelZ && posR / posZ > diagonalTan && posR > barrelR && posR < mudR ) {
+    bfield.setX( 0. );
+    bfield.setY( 0. );
+    bfield.setZ( -cornerField );
   }
   
-  // Endcap part, outer position of SOL, z position < 0, B_y direction < 0  
-  if ( position.z() < 0 && posZ > barrelZ && posZ < mudZ && posR > frontEndcapR && posR < mudR ) {
-    bfield.setY( -magField );
+  // Diagonal part to endcap part and z position > 0
+  // B_vec = B*( sin(phi), cos(phi), 0 )
+  if ( position.z() > 0  && posR / posZ < diagonalTan && posR / (mudZ-posZ) > endcapTan && posZ < mudZ && posZ > barrelZ ) {
+    bfield.setX( bEndcap.x() );
+    bfield.setY( bEndcap.y() );
+    bfield.setZ( 0. );
   }
-  
-  // Endcap part, inner radius position of the SOL
-  if ( posZ > barrelZ && posZ < mudZ && posR > innerR && posR < frontEndcapR ) {
-    bfield.setZ( magField );
+
+  // Diagonal part to endcap part and z position > 0
+  // B_vec = B*( -sin(phi), -cos(phi), 0 )
+  if ( position.z() < 0  && posR / posZ < diagonalTan && posR / (mudZ-posZ) > endcapTan && posZ < mudZ && posZ > barrelZ ) {
+    bfield.setX( -bEndcap.x() );
+    bfield.setY( -bEndcap.y() );
+    bfield.setZ( 0. );
+  }
+
+  // endcap part to frontEndcap part and z position > 0, B_z direction > 0
+  if ( position.z() > 0  && posR / (mudZ-posZ) < endcapTan && posZ > barrelZ && posR > innerR ) {
+    bfield.setX(0.);
+    bfield.setY(0.);    
+    bfield.setZ( returnField );
+  }
+
+  // endcap part to frontEndcap part and z position < 0, B_y direction < 0
+  if ( position.z() < 0  && posR / (mudZ-posZ) < endcapTan && posZ > barrelZ && posR > innerR ) {
+    bfield.setX(0.);
+    bfield.setY(0.);        
+    bfield.setY( -returnField );
   }
   
 }
 
 //=====================================================================
-void J4MUDMField::GetLocalValidBox( G4double* lpos ){
+void J4MUDMField::GetLocalValidBox( G4double* lpos ) {
 
   lpos[0] = fParameterList->GetMUDOuterR();
   lpos[1] = fParameterList->GetMUDOuterR();
