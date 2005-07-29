19e4a83110049f8484183c2bb207856dfa89bdfe
ono <>
2005-07-29 10:42:36 +0000

    2005/07/30 hiroaki ono  Add solenoid coil

--- sources/sol/include/J4SOLParameterList.hh
+++ sources/sol/include/J4SOLParameterList.hh
@@ -25,9 +25,9 @@
 class J4SOLParameterList : public J4VParameterList 
 {
  protected:
-   J4SOLParameterList(G4double ri     = 375.*cm,
-                      G4double ro     = 450.*cm,
-                      G4double len    = 380.*cm);
+   J4SOLParameterList( G4double ri     = 375.*cm,
+                       G4double ro     = 450.*cm,
+                       G4double len    = 380.*cm );
  public: 
 
    static J4SOLParameterList  *GetInstance();
@@ -35,22 +35,38 @@ class J4SOLParameterList : public J4VParameterList
    virtual ~J4SOLParameterList();
    
    // Getters
-   inline G4String GetSOLMaterial() const { return fSOLMaterial; }
    inline G4double GetSOLInnerR  () const ; 
    inline G4double GetSOLOuterR  () const ;
    inline G4double GetSOLHalfZ   () const ;
+   inline G4String GetSOLMaterial() const { return fSOLMaterial; }
    inline G4bool   GetSOLVisAtt  () const { return fSOLVisAtt;   }
    inline G4Color  GetSOLColor   () const { return fSOLColor;    }
    inline G4double GetFieldR     () const { return fFieldR;      }
    inline G4double GetBField     () const { return fBField;      }
 
+   // Get Coil parameters
+   inline G4double GetSOLCoilInnerR  () const { return fSOLCoilInnerR;   }
+   inline G4double GetSOLCoilOuterR  () const { return fSOLCoilOuterR;   }
+   inline G4double GetSOLCoilHalfZ   () const { return fSOLCoilHalfZ;    }
+   inline G4String GetSOLCoilMaterial() const { return fSOLCoilMaterial; }
+   inline G4bool   GetSOLCoilVisAtt  () const { return fSOLCoilVisAtt;   }
+   inline G4Color  GetSOLCoilColor   () const { return fSOLCoilColor;    }
+
    // Setters
-   inline void     SetSOLMaterial(G4String s) { fSOLMaterial = s; }
-   inline void     SetSOLVisAtt  (G4bool   b) { fSOLVisAtt   = b; }
-   inline void     SetSOlColor   (G4Color  c) { fSOLColor    = c; }
-   inline void     SetFieldR     (G4double x) { fFieldR      = x; }
-   inline void     SetBField     (G4double x) { fBField      = x; }
-   
+   inline void     SetSOLMaterial    ( G4String s ) { fSOLMaterial = s; }
+   inline void     SetSOLVisAtt      ( G4bool   b ) { fSOLVisAtt   = b; }
+   inline void     SetSOlColor       ( G4Color  c ) { fSOLColor    = c; }
+   inline void     SetFieldR         ( G4double x ) { fFieldR      = x; }
+   inline void     SetBField         ( G4double x ) { fBField      = x; }
+  
+   // Set Coil parameters
+   inline void     SetSOLCoilInnerR  ( G4double r ) { fSOLCoilInnerR   = r; }
+   inline void     SetSOLCoilOuterR  ( G4double r ) { fSOLCoilOuterR   = r; }
+   inline void     SetSOLCoilHalfZ   ( G4double z ) { fSOLCoilHalfZ    = z; }
+   inline void     SetSOLCoilMaterial( G4String s ) { fSOLCoilMaterial = s; }
+   inline void     SetSOLCoilVisAtt  ( G4bool   b ) { fSOLCoilVisAtt   = b; }
+   inline void     SetSOlCoilColor   ( G4Color  c ) { fSOLCoilColor    = c; }
+
    virtual void    PrintParameterList(){};
     
  private:
@@ -62,6 +78,14 @@ class J4SOLParameterList : public J4VParameterList
    G4Color   fSOLColor;
    G4double  fFieldR;
    G4double  fBField;
+
+   //Coil
+   G4double  fSOLCoilInnerR;
+   G4double  fSOLCoilOuterR;
+   G4double  fSOLCoilHalfZ;
+   G4String  fSOLCoilMaterial; 
+   G4bool    fSOLCoilVisAtt;
+   G4Color   fSOLCoilColor;
 
 };
 
@@ -82,6 +106,5 @@ G4double J4SOLParameterList::GetSOLHalfZ() const
 {
    return J4ParameterList::GetInstance()->GetSOLHalfZ();
 }
-
 
 #endif
