ccc17fc94180ad4651e14ac7564a4c7ce9b5ee36
ono <>
2005-07-29 10:40:35 +0000

    2005/07/30  hiroaki ono   Add copper

--- sources/sol/src/J4SOLMaterialStore.cc
+++ sources/sol/src/J4SOLMaterialStore.cc
@@ -43,8 +43,20 @@ G4Material* J4SOLMaterialStore::Create(const G4String& name,
     //  G4int natoms, nel, ncomponents;
     // temperature of experimental hall is controlled at 20 degree.
     //  const G4double expTemp= STP_Temperature+20.*kelvin; 
-    
+  }
+
+  G4double A, Z;
+  G4String mname;
+  G4double density;
   
+  if ( name == "Copper" ) {
+    //--------------
+    // materials...
+    //--------------
+    A       = 63.546 *g/mole;
+    density = 8.96 *g/cm3;
+    G4Material* Cu   = new G4Material( mname="Copper", Z=29., A, density );
+    material = Cu;
   }
   
   return material;
